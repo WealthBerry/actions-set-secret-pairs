c9c2d612ea544a0bcf31c99f95de405a971bc7bc
Plamen Petrov <plamen.petrov@wealthberry.com>
2022-03-09 14:51:02 +0200

    allin

--- dist/index.js
+++ dist/index.js
@@ -6636,6 +6636,56 @@ var __webpack_exports__ = {};
 const Core = __nccwpck_require__(818)
 const Api = __nccwpck_require__(95)
 
+const setSecret = async (api, secret_name, secret_value) => {
+  try {
+    const {key_id, key} = await api.getPublicKey()
+    const data = await api.createSecret(key_id, key, secret_name, secret_value)
+
+    if (api.isOrg()) {
+      data.visibility = Core.getInput('visibility')
+
+      if (data.visibility === 'selected') {
+        data.selected_repository_ids = Core.getInput('selected_repository_ids')
+      }
+    }
+
+    let response = await api.setSecret(data, secret_name)
+    console.error(response.status, response.data)
+
+    if (response.status >= 400) {
+      Core.setFailed(response.data)
+    }
+    
+    return response
+
+  } catch (e) {
+    Core.setFailed(e.message)
+    console.error(e)
+  }     
+}
+
+const getKeyPair = async (pwd = '') => {
+    return new Promise((resolve, reject) => {
+        generateKeyPair('rsa', {
+            modulusLength: 4096,
+            publicKeyEncoding: {
+                type: 'spki',
+                format: 'pem'
+            },
+            privateKeyEncoding: {
+                type: 'pkcs8',
+                format: 'pem',
+                cipher: 'aes-256-cbc',
+                passphrase: pwd
+            }
+        }, (err, publicKey, privateKey) => {
+            if (err) return reject(err);
+            resolve({publicKey, privateKey});
+        });
+    });
+};
+
+
 /**
  * Set secrets in Github repo
  * This actions is participating in #ActionsHackathon 2020
@@ -6647,51 +6697,47 @@ const Api = __nccwpck_require__(95)
  * @see https://dev.to/devteam/announcing-the-github-actions-hackathon-on-dev-3ljn
  * @see https://dev.to/habibmanzur/placeholder-title-5e62
  */
-const boostrap = async (api, secret_name, secret_value, keys) => {
-  console.log('keys', keys);
-  console.log('keys', JSON.stringify(keys));
+const boostrap = async (api, pairs) => {
+  console.log('pairs', pairs);
   
-    try {      
-      const {key_id, key} = await api.getPublicKey()
-      const data = await api.createSecret(key_id, key, secret_name, secret_value)
+  let response;
+  
+  // [ { "name": "TOKENIZE_MICROSERVICE", "public": "${{ secrets.TOKENIZE_MICROSERVICE_PUBLIC_KEY }}", "private": "${{ secrets.TOKENIZE_MICROSERVICE_PRIVATE_KEY }}" },
+  for (let pair of pairs) {
+    const publicKeyName = pair.name + "_PUBLIC_KEY";
+    const publicKeyPrevName = pair.name + "_PUBLIC_KEY_PREV";
+    
+    const privateKeyName = pair.name + "_PRIVATE_KEY";
+    const privateKeyPrevName = pair.name + "_PRIVATE_KEY_PREV";
 
-      if (api.isOrg()) {
-        data.visibility = Core.getInput('visibility')
+    console.log('xxx', 'yyy')
+    //  MOVE OLD KEY TO PREV
+    await setSecret(api, publicKeyPrevName, pair.public)
+    await setSecret(api, privateKeyPrevName, pair.private)
+    
+    // SET NEW
+    const {publicKey, privateKey} = await getKeyPair();
+    console.log({publicKey, privateKey});    
+    await setSecret(api, publicKeyName, publicKey)
+    await setSecret(api, privateKeyName, privateKey)
+  } // END OF FOR
 
-        if (data.visibility === 'selected') {
-          data.selected_repository_ids = Core.getInput('selected_repository_ids')
-        }
-      }
-
-      const response = await api.setSecret(data, secret_name)
-      console.error(response.status, response.data)
-      
-      if (response.status >= 400) {
-        Core.setFailed(response.data)
-      } else {
-        Core.setOutput('status', response.status)
-        Core.setOutput('data', response.data)
-      }
-
-    } catch (e) {
-      Core.setFailed(e.message)
-      console.error(e)
-    }   
+  Core.setOutput('status', response.status)
+  Core.setOutput('data', response.data)
+  
 }
 
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const keys = Core.getInput('keys')
-  const name = Core.getInput('name')
-  const value = Core.getInput('value')
+  const pairs = Core.getInput('pairs')
   const repository = Core.getInput('repository')
   const token = Core.getInput('token')
   const org = Core.getInput('org')
 
   const api = new Api(token, repository, !!org)
 
-  boostrap(api, name, value, keys)
+  boostrap(api, pairs)
 
 } catch (error) {
   Core.setFailed(error.message)
